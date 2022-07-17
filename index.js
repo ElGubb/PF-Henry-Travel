68b3c78424bbd445b73efdd5358136d3c2ff54c7
ArrayaAgustin <agustinarraya10@gmail.com>
2022-07-17 17:41:04 -0300

    Update  index.js branch-frontend
    prueba

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 let saludo = "hola"
 let valor = 9
 
-function saludar() {
-    console.log(saludo);
-    console.log(valor);
-    let valor = 8;
-    console.log(valor);
-}
+function saludar() 
+ {console.log("commit desde el front")}
