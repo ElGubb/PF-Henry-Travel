6d9f22d4447b28646b40d1e23869a14e31d32aba
Carlos <carloslamas3.0@gmail.com>
2022-07-17 18:15:52 -0300

    Probando

---
+++ api/index.js
@@ -0,0 +1,13 @@
+// Lista de cosas a necesitar: 
+// Sequelize, express, firebase
+// react, redux, react...
+
+//El esquema para empezar es igual de un PI:
+// Hay que elaborar la conexion del backend con con la Base de Datos(La parte del buda, relaciones,... hacemos copy and paste de cualquier PI?)
+//Luego seria crear las rutas... empezamos con el usuario, paquete, hotel, bus, ciudad y actividad
+// Deberiamos crear un archivo .json para los datos que vamos a tomar? podriamos tambien ir recopilando info, 
+//aunuque eso depende del alcance
+
+
+//En el front vendria lo mismo que el PI: ordenamientos, filtraciones, search bar y un formulario de creacion
+//Tambien hay quedarle estilos y poner un paginado
